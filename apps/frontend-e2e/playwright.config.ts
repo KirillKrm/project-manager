fb9dd9c1a481ebd1b4735a1578442f379b34a478
KirillKrm <kirillkarmazin2301@gmail.com>
2024-04-15 17:45:55 +0300

    fix

--- apps/frontend-e2e/playwright.config.ts
+++ apps/frontend-e2e/playwright.config.ts
@@ -25,9 +25,9 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx nx serve project-manager',
+    command: 'npx nx serve frontend',
     url: 'http://localhost:4200',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !process.env['CI'],
     cwd: workspaceRoot,
   },
   projects: [
